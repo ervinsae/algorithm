f706bfc98e89232b10d58e78a27a310f0f52f2cc
ervin <ervinsae@gmail.com>
2022-11-10 10:22:35 +0800

    1，线程池拒绝策略，队列的一些用法

--- src/main/java/thread/pool/SemaphoreTest.java
+++ src/main/java/thread/pool/SemaphoreTest.java
@@ -1,21 +1,25 @@
 package thread.pool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 
 public class SemaphoreTest {
 
     /* 读取文件数据的线程数量,并创建此容量的线程池 */
     private static final int THREAD_COUNT = 10;
     private static ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+    /*private static ExecutorService executorService = new ThreadPoolExecutor(THREAD_COUNT,
+            THREAD_COUNT,
+            0L,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(10),
+            new ThreadPoolExecutor.AbortPolicy());*/
 
     /* 创建Semaphore对象实例，构造函数的参数指定信号量的数目，为了方便说明问题，设为3 */
     private static Semaphore semaphore = new Semaphore(3);
 
     public static void main(String[] args){
         /* 创建线程读取数据，并尝试获取数据库连接，将数据存储到数据库中 */
-        for(int i = 0;i < THREAD_COUNT;i++){
+        for(int i = 0;i < THREAD_COUNT * 2.5;i++){
             final int index = i;
 
             Runnable task = new Runnable() {
@@ -26,9 +30,9 @@ public class SemaphoreTest {
 
                         /* 通过acquire 函数获取数据库连接，如果成功将数据存储到数据库 */
                         semaphore.acquire();
-                        System.out.println("thread-"+ index + " is saving data....");
+                        //System.out.println("thread-"+ index + " is saving data....");
                         /*模拟存储数据耗时*/
-                        Thread.sleep(10);
+                        Thread.sleep(1000);
                     }catch (InterruptedException e){
                         e.printStackTrace();
                     }finally {
---
+++ src/main/java/thread/pool/ThreadTest1.java
@@ -0,0 +1,46 @@
+package thread.pool;
+
+import java.util.concurrent.*;
+
+public class ThreadTest1 {
+
+   /* private ExecutorService executors;
+
+    public ThreadTest01() {
+        //阿里规范中不推荐使用Executors来创建线程池
+        //executors = Executors.newSingleThreadExecutor();
+
+        executors = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(500));
+    }
+
+    public ExecutorService getExecutors() {
+        return executors;
+    }
+*/
+
+    public static void main(String[] args) {
+        ExecutorService executors = new ThreadPoolExecutor(1,
+                1,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(2),
+                new ThreadPoolExecutor.DiscardOldestPolicy());
+        //ThreadTest01 test01 = new ThreadTest01();
+        for (int i = 0; i < 99; i++) {
+            int taskId = i;
+            Runnable task = () -> {
+                try {
+                    System.out.println("thread-" + Thread.currentThread().getName() + " is saving data:" + taskId);
+                    /*模拟存储数据耗时*/
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+            executors.execute(task);
+        }
+
+
+    }
+
+}
